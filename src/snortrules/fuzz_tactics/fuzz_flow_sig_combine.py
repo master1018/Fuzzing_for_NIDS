30bd8f4a6255c6a67e1a404d792300e35312fd6a
yhr <1764924903@qq.com>
2022-07-16 20:41:34 +0800

    LOG:update sig_combine

--- src/snortrules/fuzz_tactics/fuzz_flow_sig_combine.py
+++ src/snortrules/fuzz_tactics/fuzz_flow_sig_combine.py
@@ -114,7 +114,7 @@ class Signature(object):
 
 
 class FuzzStrategySigCombine(FuzzStrategyFlowBase):
-    def __init__(self, rule_file, protocols, target, max_rule_num=10, iteration=5):
+    def __init__(self, rule_file, protocols, target, max_rule_num=10, iteration=1):
         super(FuzzStrategySigCombine, self).__init__('sig_combine', rule_file, protocols, target)
         self.rule_list = self.rule_file.get_rule_set(is_active=True)  # list of rules as SnortRule
         self._combined_sig_dict = {}
@@ -159,7 +159,7 @@ class FuzzStrategySigCombine(FuzzStrategyFlowBase):
                         rule_pool = rules
                         # random chose max_rule_num rules and add them in the considered_rules
                         considered_rules.append(random.sample(rules, self.max_rule_num))
-                        rules_to_combine = [rule for rule in rule_pool if rule not in considered_rules]
+                        rules_to_combine = considered_rules[0] ##[rule for rule in rule_pool if rule not in considered_rules]
                         for rule in rules_to_combine:
                             rule_attr = SnortRuleAttr(rule)
                             rule_id = '-'.join([str(rule_attr.get_opt_gid()), str(rule_attr.get_opt_sid())])
@@ -227,7 +227,8 @@ class FuzzStrategySigCombine(FuzzStrategyFlowBase):
             if signature.range is None:
                 signature.index = len(self._combined_sig_dict[sticky_buffer])
                 ########################################################################################################################length 是个啥玩意
-                signature.start_pos = self._combined_sig_dict[sticky_buffer][0].start_pos + self._combined_sig_dict[sticky_buffer][0].length
+                
+                signature.start_pos = self._combined_sig_dict[sticky_buffer][self._combined_rule_num - 1].start_pos + self._combined_sig_dict[sticky_buffer][self._combined_rule_num - 1].length
                 self._combined_sig_dict[sticky_buffer].append(signature)
                 return True
             # if the signature does have range modifier, parse the absolute and relative modifiers in turn
@@ -384,18 +385,18 @@ if __name__== "__main__":
     fuzz_strategy.rule_selection()
     fuzz_strategy.implement_strategy()
     fuzz_strategy.fuzz_code_generation()
-    #print(fuzz_strategy._combined_rule_num)
+    print(fuzz_strategy._combined_rule_num)
     print(fuzz_strategy._combined_sig_dict)
     print(fuzz_strategy.session)
 
-    f = open("E:\\Github\\Fuzzing_for_NIDS\src\\result_analysis\\test.txt", "a+")
-    for sig in fuzz_strategy._combined_sig_dict['none']:
-        print(sig.index, file=f)
-        print(sig.pattern_str, file=f)
-        print(sig.sticky_buffer, file=f)
-        print(sig.start_pos, file=f)
-        print(sig.range, file=f)
-        print(sig.sig_type, file=f)
-        print(sig.rule_id, file=f)
-        #print("\n", file=f)
-    f.close()
+    #f = open("E:\\Github\\Fuzzing_for_NIDS\src\\result_analysis\\test.txt", "a+")
+    #for sig in fuzz_strategy._combined_sig_dict['none']:
+    #    print(sig.index, file=f)
+    #    print(sig.pattern_str, file=f)
+    #    print(sig.sticky_buffer, file=f)
+    #    print(sig.start_pos, file=f)
+    #    print(sig.range, file=f)
+    #   print(sig.sig_type, file=f)
+    #    print(sig.rule_id, file=f)
+    #    #print("\n", file=f)
+    #f.close()
